6463993bf4213953990061596df11c674c793d61
Greg Lind <gwlind@gmail.com>
2024-12-14 09:11:05 -0800

    search

--- search/views.py
+++ search/views.py
@@ -2,7 +2,6 @@ from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.template.response import TemplateResponse
 
 from wagtail.models import Page
-from wagtail.search.models import Query
 
 
 def search(request):
