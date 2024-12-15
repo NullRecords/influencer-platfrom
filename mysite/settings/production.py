fe54584f80395baba3004a3e3f062580bae397a6
Greg Lind <gwlind@gmail.com>
2024-12-15 09:26:54 -0800

    sources

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -17,7 +17,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['eileen-app-jlxek.ondigitalocean.app']
 
-CSRF_TRUSTED_ORIGINS = ['https://eileen-app-jlxek.ondigitalocean.app']
+CSRF_TRUSTED_ORIGINS = ['https://eileen-app-jlxek.ondigitalocean.app','https://cms-static.nyc3.digitaloceanspaces.com']
 
 try:
     from .local import *
