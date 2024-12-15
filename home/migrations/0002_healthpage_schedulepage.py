ead19fbce01446b231b99a520c117848f26fbd5c
Greg Lind <gwlind@gmail.com>
2024-12-15 11:26:36 -0800

    fix

--- home/migrations/0002_healthpage_schedulepage.py
+++ home/migrations/0002_healthpage_schedulepage.py
@@ -7,7 +7,6 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('wagtailcore', '0092_query_searchpromotion_querydailyhits'),
         ('home', '0001_initial'),
     ]
 
