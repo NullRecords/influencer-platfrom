35533164dad5b41cb76bb9f76e7840dc8f3bdffd
Greg Lind <gwlind@gmail.com>
2024-12-14 16:15:00 -0800

    configure

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -15,7 +15,7 @@ DATABASES = {
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['eileen-app-lrlvb.ondigitalocean.app', 'open.build', '127.0.0.1', '[::1]','www.open.build','ukraine.open.build','colombia.open.build']
+ALLOWED_HOSTS = ['eileen-app-lrlvb.ondigitalocean.app']
 
 try:
     from .local import *
