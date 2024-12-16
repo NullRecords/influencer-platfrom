5043a523259fa82d78df3fdda71688b5a8881cc3
Greg Lind <gwlind@gmail.com>
2024-12-15 16:44:58 -0800

    cleanup

--- blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from wagtail.contrib.sitemaps.views import sitemap
-from wagtail import urls as wagtail_urls
-
-urlpatterns = [
-    path("sitemap.xml", sitemap),
-    path("", include(wagtail_urls)),
-]
